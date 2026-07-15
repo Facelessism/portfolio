7fa31acd54544bea1bdc170ebea0cd41b353a8c9
facelessism <bighna2005@gmail.com>
2026-07-15 17:49:58 +0530

    set up the initial project structure with main, styles and base app components

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+function App() {
+  return (
+    <>
+
+      {/* Navigation */}
+
+      <header>
+        <nav></nav>
+      </header>
+
+      {/* Main */}
+
+      <main>
+
+        {/* Hero */}
+
+        <section id="hero"></section>
+
+        {/* Mission */}
+
+        <section id="mission"></section>
+
+        {/* Featured Product */}
+
+        <section id="featured"></section>
+
+        {/* Projects */}
+
+        <section id="projects"></section>
+
+        {/* Open Source */}
+
+        <section id="opensource"></section>
+
+        {/* Contact */}
+
+        <section id="contact"></section>
+      </main>
+
+      {/* Footer */}
+
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
