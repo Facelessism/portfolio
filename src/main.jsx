b411cf48cefccc8899fef2c34c52c147b745d829
facelessism <bighna2005@gmail.com>
2026-08-08 16:58:29 +0530

    fix: resolve production build errors

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </StrictMode>
