f178ac207327b6610128ae21cc1aed723428dadf
facelessism <bighna2005@gmail.com>
2026-09-02 23:08:50 +0530

    make repository list collapsible inside the github page

--- src/pages/GitHub.jsx
+++ src/pages/GitHub.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import PageHeader from "../components/PageHeader";
 import SectionHeader from "../components/SectionHeader";
 import Container from "../components/Container";
@@ -6,6 +8,13 @@ import GitHubOverview from "../components/GitHubOverview";
 import GitHubRepositoryList from "../components/GitHubRepositoryList";
 
 function GitHub() {
+  const [repositoriesOpen, setRepositoriesOpen] =
+    useState(false);
+
+  function toggleRepositories() {
+    setRepositoriesOpen((current) => !current);
+  }
+
   return (
     <div className="github-page">
       <PageHeader
@@ -26,9 +35,40 @@ function GitHub() {
           path="~/repositories"
           title="Everything I've Built"
           description="Developer tools, backend systems, automation projects, experiments and open-source contributions."
+          action={
+            <button
+              type="button"
+              className={`repository-list-toggle ${
+                repositoriesOpen ? "is-open" : ""
+              }`}
+              onClick={toggleRepositories}
+              aria-expanded={repositoriesOpen}
+              aria-controls="github-repositories"
+            >
+              <span>See all repositories</span>
+
+              <span
+                className="repository-list-toggle-icon"
+                aria-hidden="true"
+              >
+                <span />
+                <span />
+              </span>
+            </button>
+          }
         />
 
-        <GitHubRepositoryList />
+        <div
+          id="github-repositories"
+          className={`github-repository-list-collapse ${
+            repositoriesOpen ? "is-open" : ""
+          }`}
+          aria-hidden={!repositoriesOpen}
+        >
+          <div className="github-repository-list-collapse-inner">
+            <GitHubRepositoryList />
+          </div>
+        </div>
       </Container>
     </div>
   );
